3390340cd337a2fc9aae35ce3879e1210076aff2
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-03-10 11:24:56 -0800

    Time: 24 ms (85.37%), Space: 58.7 MB (49.98%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -13,22 +13,25 @@ class Solution {
         Arrays.sort(nums);
         for(int i = 0; i<nums.length && nums[i]<= 0;i++){
             if(i == 0 || nums[i-1] != nums[i])
-                twoSum(nums,ans, i);
+                twoSum2(nums,ans, i);
         }
         return ans;
     }
     
-    public void twoSum(int nums[], List<List<Integer>> ans, int i){
-        HashSet<Integer> comp = new HashSet<>();
-        
-        for(int j = i+1;j<nums.length;j++){
-            int complement = -nums[i] - nums[j];
-            if(comp.contains(complement)){
-                ans.add(Arrays.asList(nums[i],nums[j], complement));
-                while(j+1<nums.length && nums[j] == nums[j+1])
-                    j++;
+    public void twoSum2(int nums[], List<List<Integer>> ans, int i){
+        int l = i+1,r = nums.length-1;
+        while(l<r){
+            int sum = nums[i] + nums[l] + nums[r];
+            if(sum<0){
+                ++l;
+            }else if(sum > 0){
+                --r;
+            }else{
+                ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                l++;r--;
+                while(l<r && nums[l] == nums[l-1])
+                    ++l;
             }
-            comp.add(nums[j]);
         }
     }
 }
