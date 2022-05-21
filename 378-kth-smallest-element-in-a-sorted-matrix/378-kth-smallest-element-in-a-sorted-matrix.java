2fad4ffb3cded0062f74823765211123ea691114
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-05-21 13:55:36 -0700

    Time: 27 ms (30.80%), Space: 57 MB (42.47%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,58 @@
+class MyHeapNode {
+
+  int row;
+  int column;
+  int value;
+
+  public MyHeapNode(int v, int r, int c) {
+    this.value = v;
+    this.row = r;
+    this.column = c;
+  }
+
+  public int getValue() {
+    return this.value;
+  }
+
+  public int getRow() {
+    return this.row;
+  }
+
+  public int getColumn() {
+    return this.column;
+  }
+}
+
+class Solution {
+
+  public int kthSmallest(int[][] matrix, int k) {
+
+    int N = matrix.length;
+
+    PriorityQueue<MyHeapNode> minHeap =
+        new PriorityQueue<MyHeapNode>(Math.min(N, k), (x,y) -> x.value - y.value);
+
+    // Preparing our min-heap
+    for (int r = 0; r < Math.min(N, k); r++) {
+
+      // We add triplets of information for each cell
+      minHeap.offer(new MyHeapNode(matrix[r][0], r, 0));
+    }
+
+    MyHeapNode element = minHeap.peek();
+    while (k-- > 0) {
+
+      // Extract-Min
+      element = minHeap.poll();
+      int r = element.getRow(), c = element.getColumn();
+
+      // If we have any new elements in the current row, add them
+      if (c < N - 1) {
+
+        minHeap.offer(new MyHeapNode(matrix[r][c + 1], r, c + 1));
+      }
+    }
+
+    return element.getValue();
+  }
+}
