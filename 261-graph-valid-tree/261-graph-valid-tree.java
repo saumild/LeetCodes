5ac39632d2216df63cccaed8d16bb5ffe43aac2e
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-05-25 18:52:02 -0700

    Time: 16 ms (7.65%), Space: 48.7 MB (15.26%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        HashMap<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i =0; i<n;i++) adj.put(i, new ArrayList<>());
+        for(int[] e: edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        HashMap<Integer,Integer> parent = new HashMap<>();
+        parent.put(0,-1);
+        Stack<Integer> s = new Stack<>();
+        
+        s.push(0);
+        while(!s.isEmpty()){
+            int curr = s.pop();
+            for(int nei : adj.get(curr)){
+                if(parent.get(curr) == nei){
+                    continue;
+                }
+                if(parent.containsKey(nei)){
+                   return false; 
+                }
+                s.push(nei);
+                parent.put(nei,curr);
+            }
+        }
+        return parent.size() == n;
+    }
+}
