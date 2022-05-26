1006b7ecee6639a473f518b9093c52ce2204577e
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-05-25 19:07:46 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (86.15%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,29 +1,29 @@
 class Solution {
+    private int[] parent;
     public boolean validTree(int n, int[][] edges) {
-        if(edges.length != n-1)
-            return false;
-        HashMap<Integer, List<Integer>> adj = new HashMap<>();
-        for(int i =0; i<n;i++) adj.put(i, new ArrayList<>());
-        for(int[] e: edges){
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
-        }
+        if(edges.length != n-1) return false;
+        parent = new int[n];
         
-        Set<Integer> visited = new HashSet<>();
-        Stack<Integer> s = new Stack<>();
-        visited.add(0);
-        s.push(0);
-        while(!s.isEmpty()){
-            int curr = s.pop();
-            for(int nei : adj.get(curr)){
-                if(!visited.contains(nei)){
-                    s.push(nei);
-                    visited.add(nei);    
-                }else{
-                    continue;
-                }
+        for (int node = 0; node < n; node++) {
+            parent[node] = node;
+        }
+        for(int[] e: edges){
+            if(!union(e[0],e[1])){
+                return false;
             }
         }
-        return visited.size() == n;
+        return true;
+    }
+    
+    public boolean union(int a,int b){
+        int ra = find(a);
+        int rb = find(b);
+        
+        if(ra != rb) parent[ra] = rb;
+        return ra == rb? false: true;
+    }
+    
+    public int find(int a){
+        return parent[a] == a ? a: find(parent[a]);
     }
 }
