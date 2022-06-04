6314f7e1dcb8e43be56bcab549a944fc852a26be
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-04 11:44:30 -0700

    Time: 0 ms (100.00%), Space: 64.1 MB (63.14%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,19 +1,30 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int temp[] = new int[nums.length];
-        int n =nums.length;
-        if(k>=n)
+        int n= nums.length;
+        if(k > n)
             k = k%n;
-        for(int i=0;i< k;i++){
-            temp[i] = nums[n-k+i];
-        }
+        reverse(nums,0, n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0, n-1);
+    }
+    
+    public void reverse(int[] nums, int s, int e){
         
-        for(int i = k; i< nums.length;i++){
-            temp[i] = nums[i-k];
-        }
-        
-        for(int i =0 ;i< nums.length;i++){
-            nums[i] = temp[i];
+        while(s<e){
+            int tmp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = tmp;
+            s++;e--;
         }
     }
 }
+/*
+divide array, 
+reverse 1st and 2nd part
+reverse the whole array
+1 2 3 4 5
+k = 2
+123-> 321
+45 -> 54
+32154 -> 45123 => soln
+*/
