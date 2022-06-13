9e594f5f4b36b98c05b4f889f9e1401ebe2998a8
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-12 23:44:56 -0700

    Time: 1 ms (55.09%), Space: 42.2 MB (59.95%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -1,34 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    
-    private List<List<Integer>> solution;
-    
-    private int getHeight(TreeNode root) {
-        
-        // return -1 for null nodes
-        if (root == null) {
-            return -1;
-        }
-        
-        // first calculate the height of the left and right children
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-        
-        int currHeight = Math.max(leftHeight, rightHeight) + 1;
-        
-        if (this.solution.size() == currHeight) {
-            this.solution.add(new ArrayList<>());
-        }
-        
-        this.solution.get(currHeight).add(root.val);
-        
-        return currHeight;
-    }
-    
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    int maxHeight = -1;
     public List<List<Integer>> findLeaves(TreeNode root) {
-        this.solution = new ArrayList<>();
-        
         getHeight(root);
-        
-        return this.solution;
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i<=maxHeight;i++){
+            res.add(map.get(i));
+        }
+        return res;
     }
+    
+    public int getHeight(TreeNode root){
+        if(root == null)
+            return -1;
+        
+        int height = Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+        List<Integer> ls = map.getOrDefault(height,new ArrayList<>());
+        ls.add(root.val);
+        map.put(height, ls);
+        
+        if(maxHeight<height){
+            maxHeight = height;
+        }
+        return height;
+    }
+    
+    
 }
