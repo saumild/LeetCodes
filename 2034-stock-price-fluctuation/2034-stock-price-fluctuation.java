09f1be504b947b4b6056e1826511096b965a76a8
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-15 00:10:43 -0700

    Time: 212 ms (43.47%), Space: 174.2 MB (18.22%) - LeetHub

---
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -0,0 +1,44 @@
+class StockPrice {
+
+    public StockPrice() {
+        
+    }
+    
+    TreeMap<Integer, Integer> record = new TreeMap<>();
+    TreeMap<Integer, Set<Integer>> vals = new TreeMap<>();
+
+    public void update(int timestamp, int price) {
+        if (record.containsKey(timestamp)) {
+            int prevPrice = record.get(timestamp);
+            Set<Integer> book = vals.get(prevPrice);
+            book.remove(timestamp);
+            if (book.isEmpty()) {
+                vals.remove(prevPrice);
+            }
+        }
+        vals.putIfAbsent(price, new HashSet<>());
+        vals.get(price).add(timestamp);
+        record.put(timestamp, price);
+    }
+
+    public int current() {
+        return record.lastEntry().getValue();
+    }
+
+    public int maximum() {
+        return vals.lastKey();
+    }
+
+    public int minimum() {
+        return vals.firstKey();
+    }
+}
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice obj = new StockPrice();
+ * obj.update(timestamp,price);
+ * int param_2 = obj.current();
+ * int param_3 = obj.maximum();
+ * int param_4 = obj.minimum();
+ */
