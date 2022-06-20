12456285d5ef7e00580a805fc80d8aac4b2e773c
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-19 18:36:57 -0700

    Time: 19 ms (49.13%), Space: 41.2 MB (21.06%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -6,9 +6,7 @@ public class Solution extends VersionControl {
         int i = 1,j = n;
         while(i<=j){
             int mid = i+(j-i)/2;
-            if(isBadVersion(mid) && !isBadVersion(mid-1))
-                return mid;
-            else if(isBadVersion(mid))
+            if(isBadVersion(mid))
                 j = mid-1;
             else
                 i = mid+1;
