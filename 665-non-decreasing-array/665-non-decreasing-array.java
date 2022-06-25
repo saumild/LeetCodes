fc39ccc69b702ae765e6ad6a6e4f10dc6e7f4544
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-24 21:28:52 -0700

    Time: 1 ms (97.53%), Space: 43.3 MB (95.29%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        boolean modified = false;
-        int len = nums.length;
-        
-        for(int idx=0;idx<len-1;idx++){
-            if(nums[idx]>nums[idx+1]){
-                if(modified) return false;
+        int numViolations = 0;
+        for (int i = 1; i < nums.length; i++) {
+            
+            if (nums[i - 1] > nums[i]) {
                 
-                modified = true;
-                if(idx-1>=0&&nums[idx+1]<nums[idx-1]){
-                    idx++;
-                    if(idx+1<len&&nums[idx+1]<nums[idx-1]) return false; 
+                if (numViolations == 1) {
+                    return false;
                 }
                 
+                numViolations++;
+                
+                if (i < 2 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
             }
         }
-        
         
         return true;
     }
