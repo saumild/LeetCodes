e56bf6d090af3089a206bc53fa682f7075437fff
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-24 21:36:52 -0700

    Time: 1 ms (97.53%), Space: 43.3 MB (95.29%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -2,15 +2,11 @@ class Solution {
     public boolean checkPossibility(int[] nums) {
         int numViolations = 0;
         for (int i = 1; i < nums.length; i++) {
-            
             if (nums[i - 1] > nums[i]) {
-                
                 if (numViolations == 1) {
                     return false;
                 }
-                
                 numViolations++;
-                
                 if (i < 2 || nums[i - 2] <= nums[i]) {
                     nums[i - 1] = nums[i];
                 } else {
@@ -18,7 +14,6 @@ class Solution {
                 }
             }
         }
-        
         return true;
     }
 }
