7aea2f902e905e6d900e5d6bb377b0fb4426de66
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-30 11:26:17 -0700

    Time: 3 ms (98.32%), Space: 45.8 MB (50.00%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,43 +1,37 @@
-//Important to see code for median in O(n) avg
 class Solution {
-    public int minMoves2(int[] nums) {
-        int n = nums.length;
-        int median = findMedian(nums, 0, n - 1, n / 2);
-        int ans = 0;
-        for(int num : nums){
-            ans += Math.abs(num - median);
-        }
-        return ans;
-    }
-    
-    public int findMedian(int[] nums, int start, int end, int k){
-        if(start >= end){
-            return nums[start];
-        }
-        int left = start, right = end;
-        int mid = nums[(start + right)/2];
-        while(left <= right){
-            while(nums[left] < mid){
-                left++;
-            }
-            while(nums[right] > mid){
-                right--;
-            }
-            if(left <= right){
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
-        }
-        if(start + k <= right){
-            return findMedian(nums, start, right, k);
-        }
-        if(start + k >= left){
-            return findMedian(nums, left, end, k - (left - start));
-        }
-        return nums[right + 1];
-    }
+  public int minMoves2(int[] nums) {
+    final int n = nums.length;
+    final int median = quickSelect(nums, 0, n - 1, (n + 1) / 2);
+    int ans = 0;
+
+    for (final int num : nums)
+      ans += Math.abs(num - median);
+
+    return ans;
+  }
+
+  private int quickSelect(int[] nums, int l, int r, int k) {
+    final int randIndex = new Random().nextInt(r - l + 1) + l;
+    swap(nums, randIndex, r);
+    final int pivot = nums[r];
+
+    int nextSwapped = l;
+    for (int i = l; i < r; ++i)
+      if (nums[i] <= pivot)
+        swap(nums, nextSwapped++, i);
+    swap(nums, nextSwapped, r);
+
+    final int count = nextSwapped - l + 1;
+    if (count == k)
+      return nums[nextSwapped];
+    if (count > k)
+      return quickSelect(nums, l, nextSwapped - 1, k);
+    return quickSelect(nums, nextSwapped + 1, r, k - count);
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    final int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
 }
-//median
