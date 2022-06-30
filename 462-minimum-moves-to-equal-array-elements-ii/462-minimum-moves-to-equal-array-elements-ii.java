8436c40af5218b7b130342578efb380e7f54a944
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-06-30 10:53:36 -0700

    Time: 5 ms (66.60%), Space: 45.8 MB (50.00%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,8 +3,10 @@ class Solution {
         Arrays.sort(nums);
         int pivot = nums[nums.length/2];
         int res = 0;
-        for(int i: nums){
-            res += Math.abs(pivot - i);
+        int l = 0,r = nums.length-1;
+        while(l<r){
+            res += nums[r] - nums[l];
+            l++;r--;
         }
         return res;
     }
