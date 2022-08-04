337de67431d7fb040253a98409b9d7316f6e750d
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-08-04 16:50:22 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (27.78%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        int height = q;
+        boolean left = false;
+
+        while(height % p != 0) {
+            height += q;
+            left = !left;
+        }
+        if(left)
+            return 2;
+        else 
+            return (height/p)%2;
+    }
+}
