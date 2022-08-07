568528051bd9258586f97bf33541f8d3c05f6749
Saumil Dedhia <32071583+saumild@users.noreply.github.com>
2022-08-07 15:15:43 -0700

    Time: 0 ms (100.00%), Space: 47.7 MB (92.37%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,64 +1,21 @@
-class MyHeapNode {
-
-  int row;
-  int column;
-  int value;
-
-  public MyHeapNode(int v, int r, int c) {
-    this.value = v;
-    this.row = r;
-    this.column = c;
-  }
-
-  public int getValue() {
-    return this.value;
-  }
-
-  public int getRow() {
-    return this.row;
-  }
-
-  public int getColumn() {
-    return this.column;
-  }
-}
-
-class MyHeapComparator implements Comparator<MyHeapNode> {
-  public int compare(MyHeapNode x, MyHeapNode y) {
-    return x.value - y.value;
-  }
-}
-
 class Solution {
-
-  public int kthSmallest(int[][] matrix, int k) {
-
-    int N = matrix.length;
-
-    PriorityQueue<MyHeapNode> minHeap =
-        new PriorityQueue<MyHeapNode>(Math.min(N, k), new MyHeapComparator());
-
-    // Preparing our min-heap
-    for (int r = 0; r < Math.min(N, k); r++) {
-
-      // We add triplets of information for each cell
-      minHeap.offer(new MyHeapNode(matrix[r][0], r, 0));
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = matrix[0][0], high = matrix[m - 1][n - 1];
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            int count = 0; 
+            int j = n - 1;
+            for (int i = 0; i < m; i++) {
+                while (j >= 0 && matrix[i][j] > mid)
+                    j--;
+                count += j + 1;
+            }
+            if (count < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
     }
-
-    MyHeapNode element = minHeap.peek();
-    while (k-- > 0) {
-
-      // Extract-Min
-      element = minHeap.poll();
-      int r = element.getRow(), c = element.getColumn();
-
-      // If we have any new elements in the current row, add them
-      if (c < N - 1) {
-
-        minHeap.offer(new MyHeapNode(matrix[r][c + 1], r, c + 1));
-      }
-    }
-
-    return element.getValue();
-  }
 }
